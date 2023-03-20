35a17862ae4447d9c3afc6012654954dae11894d
xushaha <xushaha@ya.ru>
2023-03-20 15:58:01 +0300

    8th step fixes 3

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
